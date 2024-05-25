ce420c2c82de273abeb556a5f80d740135447654
TypicalW <ashmit1665@gmail.com>
2024-05-26 01:15:50 +0530

    Added a few features and fixed a bug

--- 8-Ball-Machine/8ball.py
+++ 8-Ball-Machine/8ball.py
@@ -24,33 +24,58 @@ neutral_responses = [
     "The answer is unclear.", "Think about it and ask again."
 ]
 
+repeated_responses = [
+    "Asking it again won't Change your fate son", "Sealed the Deal Already no Switching", "Cmon Are We this stubborn?",
+    "Desperate tonight, Aren't we?","I wish you tried this hard in Life.","Getting boring now","Yaaaaawwwnnnn","Sir Please step aside the line if you do not have a question",
+    "Talking to you Feels like Watching One piece on 0.5x"]
+
+
+questions = []
 question_count = 0
 
 print("Welcome to the Dark Alleys of Magic-8 Ball Arcade")
 while True:
-    question = input("Ask me a question Confused one or remain silent to teleport back\n")
+    question = input("Ask me a question, Confused one, or remain silent to teleport back\n")
     if question.strip() == "":
         print("Farewell, Then")
         print("You've Been Granted " + str(question_count) + " answers till now!")
         break
 
     question_count += 1
+    questions.append(question)
 
-    if question_count > 5:
-        response_type = random.choices(
-            ['positive', 'neutral', 'negative'],
-            weights=[1, 3, 1],
-            k=1
-        )[0]
-    else:
-        response_type = random.choice(['positive', 'neutral', 'negative'])
-
-    if response_type == "positive":
-        answer = random.choice(positive_responses)
-    elif response_type == "negative":
+    if question.lower().startswith(("why", "how")):
         answer = random.choice(negative_responses)
+    elif question.lower().startswith(("please")):
+        answer = random.choice((positive_responses))
     else:
-        answer = random.choice(neutral_responses)
+        if question_count > 100:
+            answer = "Fuck this I'm Out, I have kids you know."
+            break
+        elif question_count > 10:
+            response_type = random.choices(
+                ['positive', 'neutral', 'negative'],
+                weights=[1, 3, 5],
+                k=1
+            )[0]
+        elif question_count > 5:
+            response_type = random.choices(
+                ['positive', 'neutral', 'negative'],
+                weights=[1, 3, 1],
+                k=1
+            )[0]
+        else:
+            response_type = random.choice(['positive', 'neutral', 'negative'])
+
+        if response_type == "positive":
+            answer = random.choice(positive_responses)
+        elif response_type == "negative":
+            answer = random.choice(negative_responses)
+        else:
+            answer = random.choice(neutral_responses)
+        
+    if len(questions) > 1 and question in questions[:-1]:
+        answer = random.choice(repeated_responses)
 
     print("Fetching your Fate. . . .")
     print("The almighty Magic 8 Ball says: " + answer)
